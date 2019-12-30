a57a82f66b64e98b653348efce0e8fd4f3e0360e
sasinskil <sasinskil@gmail.com>
2019-12-30 13:56:04 +0100

    Service controller - refactoring

--- server_rest/src/main/java/info/sasinski/controller/ServiceController.java
+++ server_rest/src/main/java/info/sasinski/controller/ServiceController.java
@@ -4,15 +4,13 @@ import info.sasinski.entity.Customer;
 import info.sasinski.entity.Employee;
 import info.sasinski.entity.MotorcycleDetails;
 import info.sasinski.entity.Service;
-import info.sasinski.response.ActionResponse;
-import info.sasinski.service.impl.ServiceMotServiceImpl;
-import info.sasinski.validationResponse.ConstraintViolationsResponse;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpStatus;
+import info.sasinski.service.ServiceMotService;
+import info.sasinski.transfer.response.ActionResponse;
+import info.sasinski.transfer.response.ConstraintViolationsResponse;
+import lombok.AllArgsConstructor;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -20,188 +18,123 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@RestController
-@RequestMapping(value = "/api/service", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
-public class ServiceController {
-    private final ServiceMotServiceImpl motService;
+@AllArgsConstructor
 
-    @Autowired
-    public ServiceController(ServiceMotServiceImpl motService) {
-        this.motService = motService;
-    }
+@RestController
+@RequestMapping(
+        value = "/api/service",
+        produces = MediaType.APPLICATION_JSON_VALUE,
+        consumes = MediaType.APPLICATION_JSON_VALUE)
+public class ServiceController extends ControllerBase {
+
+    final ServiceMotService _serviceMotService;
 
     @GetMapping
-    public HttpEntity<List<Service>> getAll() {
-
-        List<Service> allServices = motService.getAllS();
-
-        if(!(allServices.isEmpty()))
-        {
-            return ResponseEntity
-                    .status(HttpStatus.OK)
-                    .body(allServices);
-        }
-
-        return ResponseEntity
-                .status(HttpStatus.NOT_FOUND)
-                .body(null);
-
+    public ResponseEntity<List<Service>> get() {
+        List<Service> list = _serviceMotService.getAll();
+        return list.isEmpty() ?
+                notFound() :
+                ok(list);
     }
 
     @GetMapping("/{id:\\d+}")
-    public HttpEntity<Service> getServiceById(@PathVariable("id") long id) {
-
-        Service getOneById = motService.getAllS()
-                .stream().filter(t -> t.getId() == id).findAny().get();
-
-        if(getOneById != null)
-        {
-            return ResponseEntity
-                    .status(HttpStatus.OK)
-                    .body(getOneById);
-        }
-
-        return ResponseEntity
-                .status(HttpStatus.NOT_FOUND)
-                .body(null);
+    public ResponseEntity<Service> byId(@PathVariable("id") long id) {
+        Service service = _serviceMotService.getById(id);
+        return service == null ?
+                notFound() :
+                ok(service);
     }
 
     @GetMapping("/{id:\\d+}/employee")
-    public HttpEntity<Employee> findEmployeeInService(@PathVariable("id") long id) {
-        Employee employee = motService.getAllS()
-                .stream().filter(t -> t.getId() == id).findAny().get().getEmployee();
-
-        if(employee != null)
-        {
-            return ResponseEntity
-                    .status(HttpStatus.OK)
-                    .body(employee);
+    public ResponseEntity<Employee> getEmployeeByServiceId(@PathVariable("id") long id) {
+        Service service = _serviceMotService.getById(id);
+        if (service != null) {
+            Employee employee = service.getEmployee();
+            if (employee != null) {
+                return ok(employee);
+            }
+            return notFound();
         }
-
-        return ResponseEntity
-                .status(HttpStatus.NOT_FOUND)
-                .body(null);
+        return notFound();
     }
 
     @GetMapping("/{id:\\d+}/customer")
-    public HttpEntity<Customer> findCustomerInService(@PathVariable("id") long id) {
-        Customer customer = motService.getAllS()
-                .stream().filter(t -> t.getId() == id).findAny().get().getCustomer();
-
-        if(customer != null)
-        {
-            return ResponseEntity
-                    .status(HttpStatus.OK)
-                    .body(customer);
+    public ResponseEntity<Customer> getCustomerByServiceId(@PathVariable("id") long id) {
+        Service serviceById = _serviceMotService.getById(id);
+        if (serviceById == null) {
+            return notFound();
+        } else {
+            Customer customer = serviceById.getCustomer();
+            if (customer == null) {
+                return notFound();
+            }
+            return ok(customer);
         }
-
-        return ResponseEntity
-                .status(HttpStatus.NOT_FOUND)
-                .body(null);
     }
 
     @GetMapping("/{id:\\d+}/motorcycleDetails")
-    public HttpEntity<MotorcycleDetails> findMotorcycleDetailsInService(@PathVariable("id") long id) {
-
-        MotorcycleDetails motorcycleDetails = motService.getAllS()
-                .stream().filter(t -> t.getId() == id).findAny().get().getMotorcycleDetails();
-
-        if(motorcycleDetails != null)
-        {
-            return ResponseEntity
-                    .status(HttpStatus.OK)
-                    .body(motorcycleDetails);
+    public ResponseEntity<MotorcycleDetails> getMotorcycleDetails(@PathVariable("id") long id) {
+        Service byId = _serviceMotService.getById(id);
+        if (byId == null) {
+            return notFound();
         }
-
-        return ResponseEntity
-                .status(HttpStatus.NOT_FOUND)
-                .body(null);
+        MotorcycleDetails details = byId.getMotorcycleDetails();
+        if (details == null) {
+            return notFound();
+        }
+        return ok(details);
     }
 
     @GetMapping("/{id:\\d+}/details")
     @ResponseBody
-    public HttpEntity<ActionResponse> serviceDetails(@PathVariable("id") long id) {
-
-        Service service = motService.getAllS()
-                .stream().filter(t -> t.getId() == id).findAny().get();
-
-        MotorcycleDetails motorcycleDetails = service.getMotorcycleDetails();
-        Customer customer = service.getCustomer();
-        Employee employee = service.getEmployee();
-
-        ActionResponse actionResponse = new ActionResponse(motorcycleDetails, employee, customer);
-
-        if (service != null)
-        {
-            return ResponseEntity
-                    .status(HttpStatus.OK)
-                    .body(actionResponse);
+    public ResponseEntity<ActionResponse> get(@PathVariable("id") long id) {
+        Service byId = _serviceMotService.getById(id);
+        if (byId == null) {
+            return notFound();
         }
 
-        return ResponseEntity
-                .status(HttpStatus.NOT_FOUND)
-                .body(null);
+        MotorcycleDetails motorcycleDetails = byId.getMotorcycleDetails();
+        Customer customer = byId.getCustomer();
+        Employee employee = byId.getEmployee();
+
+        return ok(new ActionResponse(motorcycleDetails, employee, customer));
     }
 
     @PostMapping
-    public HttpEntity saveService(@Validated @RequestBody Service service, BindingResult bindingResult) {
-
-        if(bindingResult.hasErrors())
-        {
+    public ResponseEntity<?> post(@Validated @RequestBody Service service, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
             List<String> errors = bindingResult
                     .getAllErrors()
                     .stream()
-                    .map(e -> e.getDefaultMessage())
+                    .map(DefaultMessageSourceResolvable::getDefaultMessage)
                     .collect(Collectors.toList());
 
-            ConstraintViolationsResponse responseValidateErrors = new ConstraintViolationsResponse("409","Validation failure",errors);
-
-            return ResponseEntity
-                    .status(HttpStatus.CONFLICT)
-                    .body(responseValidateErrors);
-
+            return conflict(new ConstraintViolationsResponse("409", "Validation failure", errors));
         }
 
-        motService.saveService(service);
-        return ResponseEntity.status(HttpStatus.CREATED).build();
-
+        _serviceMotService.saveService(service);
+        return created();
     }
 
     @PutMapping("/{id:\\d+}")
-    @Transactional
-    public HttpEntity updateExistService(@Validated @RequestBody Service service,BindingResult bindingResult, @PathVariable("id") long id) {
-
-        Service service1 = motService.getAllS()
-                .stream().filter(t -> t.getId() == id).findAny().get();
-
-        if(service1==null)
-        {
-            return ResponseEntity
-                    .status(HttpStatus.NOT_FOUND)
-                    .build();
+    public ResponseEntity<?> put(@Validated @RequestBody Service service, BindingResult bindingResult, @PathVariable("id") long id) {
+        Service byId = _serviceMotService.getById(id);
+        if (byId == null) {
+            return notFound();
         }
 
-        else if (bindingResult.hasErrors())
-        {
+        if (bindingResult.hasErrors()) {
             List<String> errors = bindingResult
                     .getAllErrors()
                     .stream()
-                    .map(e -> e.getDefaultMessage())
+                    .map(DefaultMessageSourceResolvable::getDefaultMessage)
                     .collect(Collectors.toList());
 
-            ConstraintViolationsResponse responseValidateErrors = new ConstraintViolationsResponse("409","Validation failure",errors);
-
-            return ResponseEntity
-                    .status(HttpStatus.CONFLICT)
-                    .body(responseValidateErrors);
-
+            return conflict(new ConstraintViolationsResponse("409", "Validation failure", errors));
         }
 
         service.setId(id);
-        motService.saveService(service);
-
-        return ResponseEntity.noContent().build();
-
+        _serviceMotService.saveService(service);
+        return noContent();
     }
-
 }
