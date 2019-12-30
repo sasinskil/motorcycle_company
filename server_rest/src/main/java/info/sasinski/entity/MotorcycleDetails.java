376b9d9fb515f689e0b8d76d560c6ac9dae580ee
sasinskil <sasinskil@gmail.com>
2019-12-30 13:05:52 +0100

    MotorcycleDetails entity - refactoring

--- server_rest/src/main/java/info/sasinski/entity/MotorcycleDetails.java
+++ server_rest/src/main/java/info/sasinski/entity/MotorcycleDetails.java
@@ -1,139 +1,46 @@
 package info.sasinski.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
+import java.io.Serializable;
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
 
 @Entity
 @Table(name = "motorcycle_details")
-public class MotorcycleDetails {
+public class MotorcycleDetails implements Serializable {
+
     @Id
-    @SequenceGenerator(name = "MotorcycleDetails_generator", sequenceName = "MotorcycleDetails_seq", allocationSize = 1)
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "MotorcycleDetails_generator")
+    @SequenceGenerator(name = "gen_motorcycle_details", sequenceName = "seq_motorcycle_details", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "gen_motorcycle_details")
     @Column(name = "id", updatable = false, nullable = false)
-    private long id;
+    Long id;
 
     @Column(precision = 10, scale = 3)
     @NotNull
-    private BigDecimal price;
+    BigDecimal price;
 
     @ManyToOne(cascade = CascadeType.PERSIST)
-    @JoinColumn(name = "Motorcycle_Id")
+    @JoinColumn(name = "motorcycle_id")
     @NotNull
     Motorcycle motorcycle;
 
     @OneToOne(mappedBy = "motorcycleDetails")
     @JsonIgnore
-    private Transaction transaction;
+    Transaction transaction;
+
     @OneToMany(mappedBy = "motorcycleDetails")
-    @Column(name = "Test_Drive", nullable = true)
+    @Column(name = "test_drive")
     @JsonIgnore
-    private List<TestDrive> testDrive;
-
-
-    public MotorcycleDetails() {
-    }
-
-    public MotorcycleDetails(BigDecimal price, Motorcycle motorcycle, Transaction transaction,
-                             List<TestDrive> testDrive) {
-
-        this.price = price;
-        this.motorcycle = motorcycle;
-        this.transaction = transaction;
-        this.testDrive = testDrive;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public BigDecimal getPrice() {
-        return price;
-    }
-
-    public void setPrice(BigDecimal price) {
-        this.price = price;
-    }
-
-    public Motorcycle getMotorcycle() {
-        return motorcycle;
-    }
-
-    public void setMotorcycle(Motorcycle motorcycle) {
-        this.motorcycle = motorcycle;
-    }
-
-    public Transaction getTransaction() {
-        return transaction;
-    }
-
-    public void setTransaction(Transaction transaction) {
-        this.transaction = transaction;
-    }
-
-    public List<TestDrive> getTestDrive() {
-        return testDrive;
-    }
-
-    public void setTestDrive(List<TestDrive> testDrive) {
-        this.testDrive = testDrive;
-    }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((motorcycle == null) ? 0 : motorcycle.hashCode());
-        result = prime * result + ((price == null) ? 0 : price.hashCode());
-        result = prime * result + ((testDrive == null) ? 0 : testDrive.hashCode());
-        result = prime * result + ((transaction == null) ? 0 : transaction.hashCode());
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        MotorcycleDetails other = (MotorcycleDetails) obj;
-        if (motorcycle == null) {
-            if (other.motorcycle != null)
-                return false;
-        } else if (!motorcycle.equals(other.motorcycle))
-            return false;
-        if (price == null) {
-            if (other.price != null)
-                return false;
-        } else if (!price.equals(other.price))
-            return false;
-        if (testDrive == null) {
-            if (other.testDrive != null)
-                return false;
-        } else if (!testDrive.equals(other.testDrive))
-            return false;
-        if (transaction == null) {
-            if (other.transaction != null)
-                return false;
-        } else if (!transaction.equals(other.transaction))
-            return false;
-        return true;
-    }
-
-    @Override
-    public String toString() {
-        return "MotorcycleDetails [id=" + id + ", price=" + price + ", motorcycle=" + motorcycle + ", transaction="
-                + transaction + ", testDrive=" + testDrive + "]";
-    }
-
-
+    List<TestDrive> testDrive = new ArrayList<>();
 }
