6071c7eacf953c94b7a408c49643892b1eeb6ff3
sasinskil <sasinskil@gmail.com>
2019-12-30 13:44:52 +0100

    CustomerController - refactoring

--- server_rest/src/main/java/info/sasinski/controller/CustomerController.java
+++ server_rest/src/main/java/info/sasinski/controller/CustomerController.java
@@ -1,11 +1,10 @@
 package info.sasinski.controller;
 
 import info.sasinski.entity.Customer;
-import info.sasinski.service.impl.CustomerServiceImpl;
-import info.sasinski.validationResponse.ConstraintViolationsResponse;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpStatus;
+import info.sasinski.service.CustomerService;
+import info.sasinski.transfer.response.ConstraintViolationsResponse;
+import lombok.AllArgsConstructor;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.BindingResult;
@@ -15,148 +14,87 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@AllArgsConstructor
+
 @RestController
 @RequestMapping(value = "/api/customer")
-public class CustomerController {
+public class CustomerController extends ControllerBase {
 
-    private final CustomerServiceImpl customerService;
-
-    @Autowired
-    public CustomerController(CustomerServiceImpl customerService) {
-        this.customerService = customerService;
-    }
+    final CustomerService _customerService;
 
     @GetMapping
-    public HttpEntity<List<Customer>> getCustomers() {
-
-        List<Customer> getAll = customerService.findAll();
-
-        if(getAll != null)
-        {
-            return ResponseEntity
-                    .status(HttpStatus.OK)
-                    .body(getAll);
-        }
-
-        return ResponseEntity
-                .status(HttpStatus.NO_CONTENT)
-                .body(null);
+    public ResponseEntity<List<Customer>> get() {
+        List<Customer> list = _customerService.findAll();
+        return list.isEmpty() ?
+                notFound() :
+                ok(list);
     }
 
     @GetMapping("/{id:\\d+}")
-    public HttpEntity<Customer> findCustomerById(@PathVariable("id") long id) {
-
-        Customer customer = customerService.findCustomerById(id);
-
-        if(customer != null)
-        {
-            return ResponseEntity
-                    .status(HttpStatus.OK)
-                    .body(customer);
-        }
-
-        return ResponseEntity
-                .status(HttpStatus.NOT_FOUND)
-                .body(null);
+    public ResponseEntity<Customer> get(@PathVariable("id") long id) {
+        Customer customerById = _customerService.findCustomerById(id);
+        return customerById == null ?
+                notFound() :
+                ok(customerById);
     }
 
     @PutMapping("/change/{id:\\d+}/{newLastName}")
-    public HttpEntity<Void> setLastNameforCustomer
-            (@PathVariable("id") long id, @PathVariable("newLastName") String newLastName)
-    {
-        Customer customer = customerService.findCustomerById(id);
-
-        if(customer != null) {
-            customerService.setLastNameforCustomer(newLastName, id);
-
-            return ResponseEntity
-                    .noContent()
-                    .build();
+    public ResponseEntity<Void> put(@PathVariable("id") long id, @PathVariable("newLastName") String newLastName) {
+        Customer customerById = _customerService.findCustomerById(id);
+        if (customerById != null) {
+            _customerService.setLastNameForCustomer(newLastName, id);
+            return noContent();
         }
-
-        return ResponseEntity
-                .status(HttpStatus.NOT_FOUND)
-                .build();
-
+        return notFound();
     }
 
     @PostMapping
-    public HttpEntity saveCustomer(@Validated @RequestBody Customer customer, BindingResult bindingResult) {
-
-
-        if(bindingResult.hasErrors())
-        {
+    public ResponseEntity<?> post(@Validated @RequestBody Customer customer, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
             List<String> errors = bindingResult
                     .getAllErrors()
                     .stream()
-                    .map(e -> e.getDefaultMessage())
+                    .map(DefaultMessageSourceResolvable::getDefaultMessage)
                     .collect(Collectors.toList());
 
-            ConstraintViolationsResponse responseValidateErrors = new ConstraintViolationsResponse("409","Validation failure",errors);
-
-            return ResponseEntity
-                    .status(HttpStatus.CONFLICT)
-                    .body(responseValidateErrors);
-
+            return conflict(new ConstraintViolationsResponse("409", "Validation failure", errors));
         }
 
-        customerService.saveCustomer(customer);
-        return ResponseEntity.status(HttpStatus.CREATED).build();
-
+        _customerService.saveCustomer(customer);
+        return created();
     }
 
     @PutMapping("/{id:\\d+}")
     @Transactional
-    public HttpEntity updateExistCustomer(@Validated @RequestBody Customer customer,BindingResult bindingResult, @PathVariable("id") long id) {
-
-        Customer customer1 = customerService.findCustomerById(id);
-
-        if(customer1==null)
-        {
-            return ResponseEntity
-                    .status(HttpStatus.NOT_FOUND)
-                    .build();
-        }
-
-        else if (bindingResult.hasErrors())
-        {
+    public ResponseEntity<?> put(@Validated @RequestBody Customer customer,
+                                 BindingResult bindingResult,
+                                 @PathVariable("id") long id) {
+        Customer customerById = _customerService.findCustomerById(id);
+        if (customerById == null) {
+            return notFound();
+        } else if (bindingResult.hasErrors()) {
             List<String> errors = bindingResult
                     .getAllErrors()
                     .stream()
-                    .map(e -> e.getDefaultMessage())
+                    .map(DefaultMessageSourceResolvable::getDefaultMessage)
                     .collect(Collectors.toList());
 
-            ConstraintViolationsResponse responseValidateErrors = new ConstraintViolationsResponse("409","Validation failure",errors);
-
-            return ResponseEntity
-                    .status(HttpStatus.CONFLICT)
-                    .body(responseValidateErrors);
-
+            return conflict(new ConstraintViolationsResponse("409", "Validation failure", errors));
         }
 
         customer.setId(id);
-        customerService.saveCustomer(customer);
+        _customerService.saveCustomer(customer);
 
-        return ResponseEntity.noContent().build();
-
+        return noContent();
     }
 
     @DeleteMapping("/{id:\\d+}")
-    public HttpEntity<Void> removeCustomer(@PathVariable("id") long id) {
-
-        Customer customer = customerService.findCustomerById(id);
-
-        if (customer != null) {
-            customerService.removeCustomer(id);
-
-            return ResponseEntity
-                    .status(HttpStatus.NO_CONTENT)
-                    .build();
+    public ResponseEntity<Void> delete(@PathVariable("id") long id) {
+        Customer customerById = _customerService.findCustomerById(id);
+        if (customerById != null) {
+            _customerService.removeCustomer(id);
+            return noContent();
         }
-
-        return ResponseEntity
-                .status(HttpStatus.NOT_FOUND)
-                .build();
-
+        return notFound();
     }
 }
