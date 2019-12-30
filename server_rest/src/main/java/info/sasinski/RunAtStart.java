07a7d59b33d473d97aaff027508e589d3520189c
sasinskil <sasinskil@gmail.com>
2019-12-30 12:42:29 +0100

    lombok configuration, addres entity - refactoring

--- server_rest/src/main/java/info/sasinski/RunAtStart.java
+++ server_rest/src/main/java/info/sasinski/RunAtStart.java
@@ -46,73 +46,73 @@ public class RunAtStart {
     @PostConstruct
     public void runAtStart() {
 
-//        List<Address> aList = new ArrayList<>();
-//        List<Employee> eList = new ArrayList<>();
-//        List<Customer> cList = new ArrayList<>();
-//        Set<Motorcycle> mSet = new HashSet<>();
-//
-//        List<MotorcycleDetails> mDList = new ArrayList<>();
-//        List<TestDrive> tDList = new ArrayList<>();
-//        List<Transaction> tList = new ArrayList<>();
-//        List<Service> sList = new ArrayList<>();
-//
-//        aList.add(new Address("Olsztyn", "65-152", "Włoskowa", 4));
-//        aList.add(new Address("Krakow", "54-332", "Koralowa", 23));
-//        aList.add(new Address("Warszawa", "23-333", "Polna", 44));
-//        aList.add(new Address("Zarzecze", "34-325", "Jasna", 11));
-//
-//        eList.add(new Employee("Janusz", "Markowski", Gender.M, LocalDate.of(1993, Month.AUGUST, 19), new BigDecimal("5000.500"),
-//                Position.DEALER, LocalDate.of(2013, Month.FEBRUARY, 20), null, aList.get(0)));
-//        eList.add(new Employee("Anna", "Kowalska", Gender.W, LocalDate.of(1982, Month.FEBRUARY, 4), new BigDecimal("2500.500"),
-//                Position.MECHANIC, LocalDate.of(2005, Month.APRIL, 10), LocalDate.of(2008, Month.APRIL, 10), aList.get(1)));
-//
-//        cList.add(new Customer("Dominik", "Wyrwak", Gender.M, LocalDate.of(1993, Month.FEBRUARY, 11), aList.get(2)));
-//        cList.add(new Customer("Ania", "Szpak", Gender.W, LocalDate.of(1994, Month.MARCH, 1), aList.get(3)));
-//
-//
-//        mSet.add(new Motorcycle(Classification.Supersport, "Suzuki", "Hayabusa GSXR 1300", "4 cylinder engine", new BigDecimal("1300.145"), 200, new BigDecimal("226.21")));
-//        mSet.add(new Motorcycle(Classification.Sport_Adventure_Tourer, "Suzuki", "V-Strom 1000", "4 cylinder engine", new BigDecimal("1037.145"), 150, new BigDecimal("160.20")));
-//
-//
-//        mDList.add(new MotorcycleDetails(new BigDecimal("45000.900"), getMotorcycle(mSet, 0), null, null));
-//        mDList.add(new MotorcycleDetails(new BigDecimal("30000.550"), getMotorcycle(mSet, 1), null, null));
-//
-//        tDList.add(new TestDrive(cList.get(0), eList.get(0), mDList.get(0), LocalDateTime.of(2018, Month.OCTOBER, 10, 14, 10), LocalDateTime.of(2018, Month.OCTOBER, 10, 16, 10)));
-//        tDList.add(new TestDrive(cList.get(1), eList.get(1), mDList.get(1), LocalDateTime.of(2018, Month.OCTOBER, 11, 13, 10), LocalDateTime.of(2018, Month.OCTOBER, 11, 14, 40)));
-//
-//
-//        tList.add(new Transaction("Purchase",new BigDecimal("50000.300"),cList.get(1),eList.get(1),mDList.get(1),
-//                LocalDateTime.of(2014,Month.APRIL,23,14,24)));
-//        tList.add(new Transaction("Purchase",new BigDecimal("40000.100"),cList.get(0),eList.get(0),mDList.get(0),
-//                LocalDateTime.of(2010,Month.OCTOBER,20,10,45)));
-//
-//
-//        sList.add(new Service("review",new BigDecimal("200.00"),LocalDate.of(2017, Month.JULY, 11),
-//                LocalDate.of(2017, Month.JULY, 12),cList.get(0),eList.get(1),mDList.get(1)));
-//        sList.add(new Service("review",new BigDecimal("200.00"),LocalDate.of(2014, Month.MAY, 21),
-//                LocalDate.of(2014, Month.MAY, 21),cList.get(1),eList.get(0),mDList.get(0)));
+        List<Address> aList = new ArrayList<>();
+        List<Employee> eList = new ArrayList<>();
+        List<Customer> cList = new ArrayList<>();
+        Set<Motorcycle> mSet = new HashSet<>();
+
+        List<MotorcycleDetails> mDList = new ArrayList<>();
+        List<TestDrive> tDList = new ArrayList<>();
+        List<Transaction> tList = new ArrayList<>();
+        List<Service> sList = new ArrayList<>();
+
+        aList.add(new Address("Olsztyn", "65-152", "Włoskowa", 4));
+        aList.add(new Address("Krakow", "54-332", "Koralowa", 23));
+        aList.add(new Address("Warszawa", "23-333", "Polna", 44));
+        aList.add(new Address("Zarzecze", "34-325", "Jasna", 11));
+
+        eList.add(new Employee("Janusz", "Markowski", Gender.M, LocalDate.of(1993, Month.AUGUST, 19), new BigDecimal("5000.500"),
+                Position.DEALER, LocalDate.of(2013, Month.FEBRUARY, 20), null, aList.get(0)));
+        eList.add(new Employee("Anna", "Kowalska", Gender.W, LocalDate.of(1982, Month.FEBRUARY, 4), new BigDecimal("2500.500"),
+                Position.MECHANIC, LocalDate.of(2005, Month.APRIL, 10), LocalDate.of(2008, Month.APRIL, 10), aList.get(1)));
+
+        cList.add(new Customer("Dominik", "Wyrwak", Gender.M, LocalDate.of(1993, Month.FEBRUARY, 11), aList.get(2)));
+        cList.add(new Customer("Ania", "Szpak", Gender.W, LocalDate.of(1994, Month.MARCH, 1), aList.get(3)));
+
+
+        mSet.add(new Motorcycle(Classification.Supersport, "Suzuki", "Hayabusa GSXR 1300", "4 cylinder engine", new BigDecimal("1300.145"), 200, new BigDecimal("226.21")));
+        mSet.add(new Motorcycle(Classification.Sport_Adventure_Tourer, "Suzuki", "V-Strom 1000", "4 cylinder engine", new BigDecimal("1037.145"), 150, new BigDecimal("160.20")));
+
+
+        mDList.add(new MotorcycleDetails(new BigDecimal("45000.900"), getMotorcycle(mSet, 0), null, null));
+        mDList.add(new MotorcycleDetails(new BigDecimal("30000.550"), getMotorcycle(mSet, 1), null, null));
+
+        tDList.add(new TestDrive(cList.get(0), eList.get(0), mDList.get(0), LocalDateTime.of(2018, Month.OCTOBER, 10, 14, 10), LocalDateTime.of(2018, Month.OCTOBER, 10, 16, 10)));
+        tDList.add(new TestDrive(cList.get(1), eList.get(1), mDList.get(1), LocalDateTime.of(2018, Month.OCTOBER, 11, 13, 10), LocalDateTime.of(2018, Month.OCTOBER, 11, 14, 40)));
+
+
+        tList.add(new Transaction("Purchase",new BigDecimal("50000.300"),cList.get(1),eList.get(1),mDList.get(1),
+                LocalDateTime.of(2014,Month.APRIL,23,14,24)));
+        tList.add(new Transaction("Purchase",new BigDecimal("40000.100"),cList.get(0),eList.get(0),mDList.get(0),
+                LocalDateTime.of(2010,Month.OCTOBER,20,10,45)));
+
+
+        sList.add(new Service("review",new BigDecimal("200.00"),LocalDate.of(2017, Month.JULY, 11),
+                LocalDate.of(2017, Month.JULY, 12),cList.get(0),eList.get(1),mDList.get(1)));
+        sList.add(new Service("review",new BigDecimal("200.00"),LocalDate.of(2014, Month.MAY, 21),
+                LocalDate.of(2014, Month.MAY, 21),cList.get(1),eList.get(0),mDList.get(0)));
 
         logger.info("======= Save some data to database =======");
 
-//
-//        for (Employee e : eList) {
-//            employeeRepository.save(e);
-//        }
-//        for (Customer c : cList) {
-//            customerRepository.save(c);
-//        }
-//        for (MotorcycleDetails m : mDList) {
-//            motorcycleDetailsRepository.save(m);
-//        }
-//        for (TestDrive tD : tDList) {
-//            testDriveRepository.save(tD);
-//        }
-//        for (Transaction t : tList) {
-//            transactionRepository.save(t);
-//        }
-//        for (Service s : sList) {
-//            serviceRepository.save(s);
-//        }
+
+        for (Employee e : eList) {
+            employeeRepository.save(e);
+        }
+        for (Customer c : cList) {
+            customerRepository.save(c);
+        }
+        for (MotorcycleDetails m : mDList) {
+            motorcycleDetailsRepository.save(m);
+        }
+        for (TestDrive tD : tDList) {
+            testDriveRepository.save(tD);
+        }
+        for (Transaction t : tList) {
+            transactionRepository.save(t);
+        }
+        for (Service s : sList) {
+            serviceRepository.save(s);
+        }
 
         //List<Employee> empLst = employeeRepository.findAll();
         //logger.info("======= Unsorted list =======");
--- server_rest/src/main/java/info/sasinski/entity/Address.java
+++ server_rest/src/main/java/info/sasinski/entity/Address.java
@@ -1,148 +1,49 @@
 package info.sasinski.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
-import javax.validation.constraints.*;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+
+@Getter
+@Setter
+@NoArgsConstructor
 
 @Entity
-public class Address{
+@Table(name = "addresses")
+public class Address implements Serializable {
 
     @Id
-    @SequenceGenerator(name = "Address_generator", sequenceName = "Address_seq", allocationSize = 1)
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "Address_generator")
+    @SequenceGenerator(name = "gen_addresses", sequenceName = "seq_addresses", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "gen_addresses")
     @Column(name = "id", updatable = false, nullable = false)
-    private long id;
+    Long id;
 
     @NotNull
-    @Size(min = 4, max = 20,message = "locality size must be between {min} and {max}")
-    protected String locality;
-    @Column(name = "Zip_code")
+    @Size(min = 4, max = 20, message = "locality size must be between {min} and {max}")
+    String locality;
+
+    @Column(name = "zip_code")
     @NotNull
-    @Pattern(regexp = "\\d{2}-\\d{3}",message = "locality zip code must contain pattern " + "\\d{2}-\\d{3}")
-    protected String zipCode;
+    @Pattern(regexp = "\\d{2}-\\d{3}", message = "locality zip code must contain pattern " + "\\d{2}-\\d{3}")
+    String zipCode;
+
     @NotNull
-    @Size(min = 3, max = 20,message = "street size must be between {min} and {max}")
-    protected String street;
-    @Column(name = "Street_number")
-    @Min(value = 1,message = "street number must be greater than or equal to {value}")
-    protected int streetNumber;
+    @Size(min = 3, max = 20, message = "street size must be between {min} and {max}")
+    String street;
+
+    @Column(name = "street_number")
+    @Min(value = 1, message = "street number must be greater than or equal to {value}")
+    Integer streetNumber;
 
     @OneToOne(mappedBy = "address")
     @JsonIgnore
-    protected Person person;
-
-    public Address() {
-    }
-
-    public Address(String locality, String zipCode, String street, int streetNumber) {
-        this.locality = locality;
-        this.zipCode = zipCode;
-        this.street = street;
-        this.streetNumber = streetNumber;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-
-    public Person getPerson() {
-        return person;
-    }
-
-    public void setPerson(Person person) {
-        this.person = person;
-    }
-
-    public String getLocality() {
-        return locality;
-    }
-
-    public void setLocality(String locality) {
-        this.locality = locality;
-    }
-
-    public String getZipCode() {
-        return zipCode;
-    }
-
-    public void setZipCode(String zipCode) {
-        this.zipCode = zipCode;
-    }
-
-    public String getStreet() {
-        return street;
-    }
-
-    public void setStreet(String street) {
-        this.street = street;
-    }
-
-    public int getStreetNumber() {
-        return streetNumber;
-    }
-
-    public void setStreetNumber(int streetNumber) {
-        this.streetNumber = streetNumber;
-    }
-
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((locality == null) ? 0 : locality.hashCode());
-        result = prime * result + ((person == null) ? 0 : person.hashCode());
-        result = prime * result + ((street == null) ? 0 : street.hashCode());
-        result = prime * result + streetNumber;
-        result = prime * result + ((zipCode == null) ? 0 : zipCode.hashCode());
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Address other = (Address) obj;
-        if (locality == null) {
-            if (other.locality != null)
-                return false;
-        } else if (!locality.equals(other.locality))
-            return false;
-        if (person == null) {
-            if (other.person != null)
-                return false;
-        } else if (!person.equals(other.person))
-            return false;
-        if (street == null) {
-            if (other.street != null)
-                return false;
-        } else if (!street.equals(other.street))
-            return false;
-        if (streetNumber != other.streetNumber)
-            return false;
-        if (zipCode == null) {
-            if (other.zipCode != null)
-                return false;
-        } else if (!zipCode.equals(other.zipCode))
-            return false;
-        return true;
-    }
-
-    @Override
-    public String toString() {
-        return "Address [locality=" + locality + ", zipCode=" + zipCode + ", street=" + street + ", streetNumber="
-                + streetNumber + "]";
-    }
-
-
+    Person person;
 }
