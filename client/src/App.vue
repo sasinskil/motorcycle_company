586e0907fba6ba48ece392c26a89c6c263262deb
sasinskil <sasinskil@gmail.com>
2020-02-16 11:55:04 +0100

    Rebuild client app

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,43 +1,32 @@
 <template>
-  <div id="app" class="app">
-    <app-header></app-header>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
     <router-view/>
   </div>
 </template>
 
-<script>
-import header from '@/components/Header.vue';
-
-export default {
-  name: 'app',
-  components: {
-    'app-header': header,
-  },
-};
-</script>
-
-
 <style lang="scss">
-
-* {
-  box-sizing: border-box;
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
 }
 
-body {
-  margin: 0;
-  padding: 0;
-}
+#nav {
+  padding: 30px;
 
-.app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-}
+  a {
+    font-weight: bold;
+    color: #2c3e50;
 
-
-@media only screen and (max-width: 640px) {
-  body {
-    font-size: 12px;
+    &.router-link-exact-active {
+      color: #42b983;
+    }
   }
 }
 </style>
--- client/src/main.js
+++ client/src/main.js
@@ -1,39 +1,12 @@
 import Vue from 'vue';
-import vueResource from 'vue-resource';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import {
-  faEraser, faTrashAlt, faSignOutAlt, faPlus, faUser, faLock,
-} from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
-
-library.add(
-  faTrashAlt, faEraser, faSignOutAlt, faPlus, faUser, faLock,
-);
-
-Vue.component('font-awesome-icon', FontAwesomeIcon);
-
-Vue.use(vueResource);
-
 Vue.config.productionTip = false;
-
-Vue.http.interceptors.push((request, next) => {
-  if (localStorage.getItem('token')) {
-    request.headers.set('Authorization', 'Bearer ' + localStorage.getItem('token'));
-  }
-
-  next((response) => {
-    if (response.status === 400 || response.status === 401 || response.status === 403) {
-      router.push({ path: '/login' });
-    }
-  });
-});
 
 new Vue({
   router,
   store,
-  render: h => h(App),
+  render: (h) => h(App),
 }).$mount('#app');
