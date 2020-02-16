a01ed7d7a2cd6da92acd833bc7dceeba315eb09d
sasinskil <sasinskil@gmail.com>
2020-02-16 13:19:58 +0100

    Rebuild client app

--- client/src/main.js
+++ client/src/main.js
@@ -1,12 +1,18 @@
-import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faSpinner } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
+
+library.add(faSpinner)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 new Vue({
   router,
   store,
-  render: (h) => h(App),
-}).$mount('#app');
+  render: h => h(App)
+}).$mount('#app')
